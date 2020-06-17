b3991c62e4eaa12a31cec1ad93447e81e435f3dc
CShell <shell.calvin7@gmail.com>
2020-06-17 12:12:04 -0600

    Updates

---
+++ precache-manifest.aebdf593fc400096ad43f1c0c00c4a41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54613d9e9fd9d888fb5bbaa8c05e4a2c",
+    "url": "/spotify-advanced-search-and-playlist-creator/index.html"
+  },
+  {
+    "revision": "1ab55efd94eb7671e96e",
+    "url": "/spotify-advanced-search-and-playlist-creator/static/js/2.bf183f6b.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/spotify-advanced-search-and-playlist-creator/static/js/2.bf183f6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a234daa09a454f69c253",
+    "url": "/spotify-advanced-search-and-playlist-creator/static/js/main.f22d7315.chunk.js"
+  },
+  {
+    "revision": "89dc78fd0794c193c517",
+    "url": "/spotify-advanced-search-and-playlist-creator/static/js/runtime-main.a66d1936.js"
+  }
+]);
