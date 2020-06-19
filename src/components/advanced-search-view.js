2ae57cfde4de1851f05ad6b249fe4b9a0e759e72
CShell <shell.calvin7@gmail.com>
2020-06-18 22:48:43 -0600

    Update advanced-search-view.js

--- src/components/advanced-search-view.js
+++ src/components/advanced-search-view.js
@@ -267,7 +267,7 @@ export const AdvancedSearchView = () => {
       if (data?.next) handleFetchActivePlaylistRemaining(data)
       else setActivePlaylist(data)
     })
-  }, [activePlaylistId, fetchData, setActivePlaylist])
+  }, [activePlaylistId, fetchData, setActivePlaylist, handleFetchActivePlaylistRemaining])
 
   const handleFetchActivePlaylistRemaining = (data) => {
     const { next, items: previousItems } = data
