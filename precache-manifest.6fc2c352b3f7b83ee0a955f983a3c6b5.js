1b068cc1e28a29eaf617a52a8229b96ef9a8f137
CShell <shell.calvin7@gmail.com>
2020-06-18 20:56:24 -0600

    Updates

---
+++ precache-manifest.6fc2c352b3f7b83ee0a955f983a3c6b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f439393cca6b07e4136de06b589ea4d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60e299a5b850c981e233",
+    "url": "/static/js/2.a2922b71.chunk.js"
+  },
+  {
+    "revision": "6b3248a1692933689dcb27b9e1ed5092",
+    "url": "/static/js/2.a2922b71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd3808e7526ac2ea31b2",
+    "url": "/static/js/main.a78aa855.chunk.js"
+  },
+  {
+    "revision": "f9cef65b87322ced2252",
+    "url": "/static/js/runtime-main.d3d7bd4e.js"
+  },
+  {
+    "revision": "31bad8d4fc4413d54f8c19930d3739b9",
+    "url": "/static/media/spotify-logo.31bad8d4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/advancedsearchforspotify.info/precache-manifest.4332dc699d7d9221ba3e3ec6d6431e3a.js"
+  "/precache-manifest.6fc2c352b3f7b83ee0a955f983a3c6b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/advancedsearchforspotify.info/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
